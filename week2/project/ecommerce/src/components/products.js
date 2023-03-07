4acc1cc55a26b0b411eae2106346166aa1aeada6
yusufelik <ysfelik1@gmail.com>
2023-03-07 01:48:25 +0100

    filtered products by backend

--- week2/project/ecommerce/src/components/products.js
+++ week2/project/ecommerce/src/components/products.js
@@ -4,28 +4,33 @@ import React, { useEffect, useState } from 'react';
 const Products = ({ category }) => {
 
   const [allProducts, setProducts] = useState([]);
+
   useEffect(() => {
+   function getURL(category) {
+    let myURL = 'https://fakestoreapi.com/products';
+    if (category !== 'all') {
+     myURL = `https://fakestoreapi.com/products/category/${category}`;
+    }
+    return myURL;
+   }
+
+  
     const fetchData = async () => {
       try {
-        const response = await fetch('https://fakestoreapi.com/products');
+        const response = await fetch(getURL(category));
         const data = await response.json();
         setProducts(data);
       } catch (error) {
         console.log(error);
       }
-    };
+    }; 
     fetchData();
-  }, []);
+  }, [category]);
 
-  let filteredProducts = allProducts;
-  if (category !== 'all') {
-    filteredProducts = allProducts.filter((product) => {
-      return product.category === category;
-    });
-  }
+
   return (
     <div className="cards">
-      {filteredProducts.map((product) => {
+      {allProducts.map((product) => {
         return <Product key={product.id} {...product} />;
       })}
     </div>
