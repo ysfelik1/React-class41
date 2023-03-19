cd18f303dd56161f44b51659f53f2a0e6fd3afcc
yusufelik <ysfelik1@gmail.com>
2023-03-19 21:45:06 +0100

    fix problems

--- week3/project/ecommerce/src/components/productDetail.js
+++ week3/project/ecommerce/src/components/productDetail.js
@@ -2,25 +2,31 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Loader from './loader';
 import Favorite from './favorite';
-import allProducts from '../fake-data/all-products';
+import ErrorPage from './errorPage';
 
 const ProductDetail = () => {
   const { id } = useParams();
   const [product, setProduct] = useState(null);
+  const [error, setError] = useState(null);
   useEffect(() => {
     const fetchData = async () => {
       try {
-        //const response = await fetch(`https://fakestoreapi.com/products/${id}`);
-       // const data = await response.json();
-       const data = allProducts;
+        const response = await fetch(`https://fakestoreapi.com/products/${id}`);
+        const data = await response.json();
         setProduct(data);
       } catch (error) {
         console.log(error);
+        setError(error);
       }
     };
     fetchData();
   }, [id]);
 
+  if (error) {
+    return (
+      <ErrorPage errorText={error.message} />
+    );
+  }
   if (!product) {
     return <Loader />;
   }
@@ -29,7 +35,6 @@ const ProductDetail = () => {
     <div className="product-detail">
       <h3>{product.title}</h3>
       <Favorite productId={product.id} ></Favorite>
-
       <div className="product-info">
         <img src={product.image} alt={product.title} />
         <p>{product.description}</p>
