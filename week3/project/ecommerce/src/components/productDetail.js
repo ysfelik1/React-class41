51aa39f44b8f13dfb6c8012dafbed69132e34be9
yusufelik <ysfelik1@gmail.com>
2023-03-19 21:31:35 +0100

    added error page

--- week3/project/ecommerce/src/components/productDetail.js
+++ week3/project/ecommerce/src/components/productDetail.js
@@ -2,17 +2,17 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Loader from './loader';
 import Favorite from './favorite';
+import allProducts from '../fake-data/all-products';
 
 const ProductDetail = () => {
   const { id } = useParams();
   const [product, setProduct] = useState(null);
-
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(`https://fakestoreapi.com/products/${id}`);
-        const data = await response.json();
-        console.log(data);
+        //const response = await fetch(`https://fakestoreapi.com/products/${id}`);
+       // const data = await response.json();
+       const data = allProducts;
         setProduct(data);
       } catch (error) {
         console.log(error);
